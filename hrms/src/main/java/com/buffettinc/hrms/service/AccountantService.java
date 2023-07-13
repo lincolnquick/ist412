0273bdb9ce547677773d109b32d19f2574c990ae
Lincoln Quick <lincolnmq@gmail.com>
2023-07-13 13:27:31 -0400

    Added Accountant and Employee Service and Service Implementations

---
+++ hrms/src/main/java/com/buffettinc/hrms/service/AccountantService.java
@@ -0,0 +1,24 @@
+package com.buffettinc.hrms.service;
+
+import com.buffettinc.hrms.model.Accountant;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Interface for {@link Accountant} service, containing methods for performing CRUD operations on Accountant objects.
+ *
+ * @author IST 412 Group 5
+ * @version 1.0
+ * @since 2023-07-13
+ */
+public interface AccountantService {
+
+    Accountant saveAccountant(Accountant accountant);
+    List<Accountant> getAllAccountants();
+    Accountant getAccountantById(UUID id);
+    Accountant updateAccountant(Accountant accountant);
+    void deleteAccountant(UUID id);
+    Page<Accountant> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
---
+++ hrms/src/main/java/com/buffettinc/hrms/service/AccountantServiceImpl.java
@@ -0,0 +1,81 @@
+package com.buffettinc.hrms.service;
+
+import com.buffettinc.hrms.model.Accountant;
+import com.buffettinc.hrms.repository.AccountantRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Implementation for {@link AccountantService} interface, containing methods for performing CRUD operations on
+ * Accountant objects.
+ *
+ * @author IST 412 Group 5
+ * @version 1.0
+ * @since 2023-07-13
+ */
+@Service
+public class AccountantServiceImpl implements AccountantService {
+
+    private final AccountantRepository accountantRepository;
+
+    public AccountantServiceImpl(AccountantRepository accountantRepository) {
+        this.accountantRepository = accountantRepository;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Accountant saveAccountant(Accountant accountant) {
+        return this.accountantRepository.save(accountant);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Accountant> getAllAccountants() {
+        return this.accountantRepository.findAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Accountant getAccountantById(UUID id) {
+        return this.accountantRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Accountant updateAccountant(Accountant accountant) {
+        return this.accountantRepository.save(accountant);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void deleteAccountant(UUID id) {
+        this.accountantRepository.deleteById(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Page<Accountant> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+
+        PageRequest pageRequest = PageRequest.of(pageNo - 1, pageSize, sort);
+        return this.accountantRepository.findAll(pageRequest);
+    }
+}
