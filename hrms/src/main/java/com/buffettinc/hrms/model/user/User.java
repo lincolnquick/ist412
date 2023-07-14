f792f46879d485650f06ce96c8278842980057cf
Lincoln Quick <lincolnmq@gmail.com>
2023-07-13 20:09:20 -0400

    Added WebSecurity and login. TODO: fully implement login

--- hrms/src/main/java/com/buffettinc/hrms/model/user/User.java
+++ hrms/src/main/java/com/buffettinc/hrms/model/user/User.java
@@ -1,2 +1,59 @@
-package com.buffettinc.hrms.model.user;public class User {
+package com.buffettinc.hrms.model.user;
+
+import jakarta.persistence.*;
+
+/**
+ * This class represents a User of the Buffett Inc. HRMS.
+ * Each User has an ID and a username and password.
+ *
+ * @author IST 412 Group 5
+ * @version 1.0
+ * @since 2023-07-13
+ */
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+        this.username = null;
+        this.password = null;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- hrms/src/main/java/com/buffettinc/hrms/repository/user/UserRepository.java
+++ hrms/src/main/java/com/buffettinc/hrms/repository/user/UserRepository.java
@@ -1,2 +1,17 @@
-package com.buffettinc.hrms.repository.user;public interface UserRepository {
+package com.buffettinc.hrms.repository.user;
+
+import com.buffettinc.hrms.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * This is the repository interface for the {@link User} entity.
+ * It extends {@link JpaRepository} to provide methods for CRUD operations.
+ * Additional query methods can be defined here as needed.
+ *
+ * @author IST 412 Group 5
+ * @version 1.0
+ * @since 2023-07-13
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
