fd1c62d0ceac1a5a0632982a2847bf2aebe28085
cheserpoo <adw139@psu.edu>
2023-08-08 20:35:33 -0400

    User and Kong users have PTO data present

--- src/main/java/com/buffettinc/hrms/service/pto/PTORequestServiceImpl.java
+++ src/main/java/com/buffettinc/hrms/service/pto/PTORequestServiceImpl.java
@@ -97,7 +97,6 @@ public class PTORequestServiceImpl implements PTORequestService {
 
     @Override
     public List<PTORequest> getPTORequestByEmployee(Long employeeID){
-        System.out.println("here");
         return ptoRequestRepository.getPTORequestByEmployee(employeeID);
     }
 }
